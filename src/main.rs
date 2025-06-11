0472a48f1f1a39535ca6cef0b071cd3b0653d8b1
Brian Martin <brayniac@gmail.com>
2025-06-11 14:37:00 -0700

    basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::time::Instant;
+use std::hint::black_box;
+
 fn main() {
-    println!("Hello, world!");
+    perf_counter_test("APERF", Msr::new(MsrId::APERF), 0);
+}
+
+fn perf_counter_test(name: &'static str, event: impl Event, core: usize) {
+    match perf_event::Builder::new(event)
+        .one_cpu(core)
+        .any_pid()
+        .exclude_hv(false)
+        .exclude_kernel(false)
+        .pinned(true)
+        .read_format(
+            ReadFormat::TOTAL_TIME_ENABLED | ReadFormat::TOTAL_TIME_RUNNING | ReadFormat::GROUP,
+        )
+        .build()
+    {
+        Ok(counter) => {
+            let iterations = 500_000;
+            let start = Instant::now();
+
+            for _ in 0..iterations {
+                black_box(counter.value());
+            }
+
+            let latency = start.elapsed().as_nanos() / iterations;
+
+            println!("counter: {} latency: {} ns/iter", name, latency);
+        }
+        Err(e) => {
+            Err(())
+        }
+    }
 }
